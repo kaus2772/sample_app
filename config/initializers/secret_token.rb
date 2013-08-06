ff0dff74ee59c8f6aa3d3ca131bf8e2ff6ab6be0
kaus <kaus2772@gmail.com>
2013-08-06 21:12:30 +0530

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Adhear::Application.config.secret_key_base = '8c070a7dd5467e3cd51fe9c13f5ad9af0e0b0fa4382ef238db988b6485986a7556094f61df74bc47a1dcdba5bf1e2a3ae701f467ded92488530213c793ed13ca'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Adhear::Application.config.session_store :cookie_store, key: '_adhear_session'
